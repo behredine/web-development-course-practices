7889cc837424b5909f197173879f98868698737a
Behredin <behredineshetu27@gmail.com>
2026-03-11 09:29:29 +0300

    "get method in express"

--- 3.1-express-server/index.js
+++ 3.1-express-server/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 const app = express();
 const PORT = 3000;
+app.get("/", (req, res) => {
+    res.send("this is testing , testing 1-2-3")
+});
+app.get("/about", (req, res) => {
+    res.send("<h1>about me</h1>");
 
+});
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Page</h1>");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
