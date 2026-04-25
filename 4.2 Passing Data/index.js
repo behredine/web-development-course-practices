0476d7f3cd4c05eb02aa26130b797cbfd81617ea
Behredin <behredineshetu27@gmail.com>
2026-04-25 11:05:14 +0300

    finalize EJS , javascript templating language

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -6,9 +6,21 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs",
+    { notice: "Enter your name below" }
+  )
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  let firstN = req.body["fName"];
+  let lastN = req.body["lName"];
+  console.log(firstN)
+  let count = firstN.length + lastN.length;
+  res.render("index.ejs",
+    { notice: `There are ${count} letters in your name!` }
+  )
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
